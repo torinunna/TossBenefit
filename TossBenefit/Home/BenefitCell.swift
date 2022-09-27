49d4c66aa5fa3e1ffeab484cdd1c979ff2f3793a
torinunna <torinunna9588@gmail.com>
2022-09-27 16:45:24 +0900

    add outlets to VC

--- TossBenefit/Home/BenefitCell.swift
+++ TossBenefit/Home/BenefitCell.swift
@@ -9,7 +9,14 @@ import UIKit
 
 class BenefitCell: UICollectionViewCell {
     
+    
+    @IBOutlet weak var benefitImage: UIImageView!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var titleLabel: UILabel!
+    
     func configure(item: Benefit) {
-        
+        benefitImage.image = UIImage(named: item.imageName)
+        descriptionLabel.text = item.description
+        titleLabel.text = item.title
     }
 }

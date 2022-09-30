5a3dd8e37d5f786eb515aa4b07fc2dbc4dbf7f96
torinunna <torinunna9588@gmail.com>
2022-09-30 17:40:18 +0900

    create BenefitGuideView and build UI programmatically

--- TossBenefit/Benefit/ButtonBenefitViewController.swift
+++ TossBenefit/Benefit/ButtonBenefitViewController.swift
@@ -13,16 +13,37 @@ class ButtonBenefitViewController: UIViewController {
     var benefitDetails: BenefitDetails = .default
 
     @IBOutlet weak var ctaButton: UIButton!
+    @IBOutlet weak var vStackView: UIStackView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        ctaButton.layer.masksToBounds = true
-        ctaButton.layer.cornerRadius = 5
-        navigationItem.largeTitleDisplayMode = .never
+       
+        setUpUI()
+        addGuides()
         ctaButton.setTitle(benefit.ctaTitle, for: .normal)
 
     }
  
+    private func setUpUI() {
+        ctaButton.layer.masksToBounds = true
+        ctaButton.layer.cornerRadius = 5
+        navigationItem.largeTitleDisplayMode = .never
+    }
 
+    private func addGuides() {
+        let guideViews: [BenefitGuideView] = benefitDetails.guides.map { guide in
+            let guideView = BenefitGuideView(frame: .zero)
+            guideView.icon.image = UIImage(systemName: guide.iconName)
+            guideView.title.text = guide.guide
+            return guideView
+        }
+        
+        guideViews.forEach{ view in
+            self.vStackView.addArrangedSubview(view)
+            NSLayoutConstraint.activate([
+                view.heightAnchor.constraint(equalToConstant: 60)
+            ])
+            
+        }
+    }
 }
